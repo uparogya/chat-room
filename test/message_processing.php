473fb9b02adb8634f1c85b284415a68c5eb8b8e6
Arogya <109647530+uparogya@users.noreply.github.com>
2022-11-01 05:53:24 +0545

    deleted test folder

--- test/message_processing.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$name = $_POST['name'];
-	$connection = new mysqli("localhost", "arogya", "verystrongpassword", "chat");
-	$inserting_query = "INSERT INTO `TEST` (`MESSAGE`) VALUES('" . $name . "');";
-	$connection -> query($inserting_query);
-?>
